6f7c5896fdd6021912d210893400cb83cf249a49
Aakash Kavediya <kavediyaaakash@gmail.com>
2025-07-26 12:49:59 +0530

    minor changes instyling

--- components/ContactComponent/footer/footer.jsx
+++ components/ContactComponent/footer/footer.jsx
@@ -82,10 +82,10 @@ const Footer = () => {
                     <li style={{listStyle:'none'}}><Link href=""><FaLinkedin size={24} color="black" /></Link></li>
                     <li style={{listStyle:'none'}}><Link href=""><FaXTwitter size={24} color="black" /></Link></li>
                 </ul><br />
-                <ul>
-                  <li style={{display:'flex',flexDirection:'row'}} ><b>Phone no: </b><p style={{marginLeft:5}}>98921 68052</p></li>
-                  <li style={{display:'flex',flexDirection:'row',alignItems:'center',marginTop:8}} ><b>email ID: </b><code style={{marginLeft:5,color:'#6899e1',fontSize:18}}>somaiyahamclub@gmail.com</code></li>
-                </ul>
+                <ol>
+                  <li style={{display:'flex',flexDirection:'row'}} ><b>Phone no: </b><p style={{marginLeft:5}}>98921 68052 (Dr.Umesh Shinde) </p></li>
+                  <li style={{display:'flex',flexDirection:'row',alignItems:'center',marginTop:8}} ><b>email ID: </b><code style={{marginLeft:5,color:'#333333',fontSize:18}}>somaiyahamclub@gmail.com</code></li>
+                </ol>
             </div>
             <div id="contactDetail">
 
